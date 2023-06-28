ecd7d5c9b0c7fce353b6b55904406979f84b4591
Manuvoc <vitoriamanueli@gmail.com>
2023-06-28 18:53:51 -0300

    Dashboard

--- resources/views/base/dashboard.blade.php
+++ resources/views/base/dashboard.blade.php
@@ -1,28 +1,15 @@
 @extends('base.app')
 @section('conteudo')
 @section('tituloPagina', 'Princípal')
-<div class="col" style="padding: 5%">
-    <div class="row">
-        <div class="col-sm-6 mb-3 mb-sm-0">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Titulo 01</h5>
-                    <p class="card-text">Descrição aqui</p>
-                    <a href="#" class="btn btn-primary">Clique aqui</a>
-                </div>
-                
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Titulo 02</h5>
-                    <p class="card-text">Descrição aqui</p>
-                    <a href="#" class="btn btn-primary">Clique aqui</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+                @php
+    $nome_imagem = !empty($dashboard->imagem) ? $dashboard->imagem : 'juntassomosgigante.jpg';
+@endphp
+<br>
+<h2>Selecione opção no cabeçalho</h2>
+    <img class="img-thumbnail" src="/storage/{{ $nome_imagem }}" width="2000px" />
+<br><br>
+
+
 
 @endsection
